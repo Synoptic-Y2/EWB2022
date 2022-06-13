cb5aaad8286e780fd2209bb5c10850c91b0b0cdd
sdq20syu <98755930+sdq20syu@users.noreply.github.com>
2022-06-13 15:27:04 +0100

    Express-Layouts

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 
 // Initialization
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts');
 const app = express()
 const passport = require('passport')
 const flash = require('express-flash')
@@ -23,8 +24,9 @@ initializePassport(
 )
 
 app.set('view-engine', 'ejs')
+app.use(expressLayouts);
 app.use(express.static('public'))
-app.use(express.urlencoded({extended: false})) // https://stackoverflow.com/questions/23259168/what-are-express-json-and-express-urlencoded
+app.use(express.urlencoded({ extended: false })) // https://stackoverflow.com/questions/23259168/what-are-express-json-and-express-urlencoded
 app.use(flash())
 app.use(session({
     secret: 'secret',
@@ -41,12 +43,12 @@ let users = JSON.parse(rawdata);
 
 /************ Home - Home Page ************/
 app.get('/', checkAuthenticated, (req, res) => {
-    res.render('Home.ejs')
+    res.render('Home.ejs', { layout: './pages/_Info.ejs', title: 'Home' })
 })
 
 /************ Login/Logout ************/
 app.get('/login', checkNotAuthenticated, (req, res) => {
-    res.render('Login.ejs')
+    res.render('Login.ejs', { layout: './pages/_Info.ejs', title: 'Login' })
 })
 
 // The login POST request will be handled in passport-config.js
@@ -63,30 +65,39 @@ app.delete('/logout', (req, res) => {
 
 /************ Register ************/
 app.get('/register', checkNotAuthenticated, (req, res) => {
-    res.render('Register.ejs')
+    res.render('Register.ejs', { layout: './pages/_Info.ejs', title: 'Register' })
+})
+app.use(expressLayouts);
+app.set("layout", "./pages/_Info");
+app.set("view engine", "ejs");
+
+
+/************ Other Pages ************/
+app.get('/info', (req, res) => {
+    res.render('Info.ejs', { layout: './pages/_Info.ejs', title: 'Sign Up' })
 })
 
 app.post('/register', checkNotAuthenticated, async (req, res) => {
-	try{
-		const hPass = await bcrypt.hash(req.body.password, 10)
-		users.push({
-			id: Date.now().toString(),
-			username:req.body.username,
-			email:req.body.email,
-			password:hPass,
-			address:req.body.address,
-			priv: req.body.priv
-		})
-		let userdata = JSON.stringify(users, undefined, 4)
-		fs.writeFileSync ('public/data/users.json', userdata)
-		res.redirect('/login')
-	}
+    try {
+        const hPass = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            username: req.body.username,
+            email: req.body.email,
+            password: hPass,
+            address: req.body.address,
+            priv: req.body.priv
+        })
+        let userdata = JSON.stringify(users, undefined, 4)
+        fs.writeFileSync('public/data/users.json', userdata)
+        res.redirect('/login')
+    }
     catch {
         res.redirect('/register')
     }
 })
 
-/************ Helper Functions ************/ 
+/************ Helper Functions ************/
 function checkAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
         return next()
